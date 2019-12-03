11cbd9654f5a637948f5b83ba0b532a44690abbe
GuhanPeriyasamy <57369215+GuhanPeriyasamy@users.noreply.github.com>
2019-12-03 13:13:46 -0600

    Update Runner.java

--- Runner.java
+++ Runner.java
@@ -8,11 +8,25 @@ public class Runner {
 	{
 		board = new Board();
 		g = new GraphicsClass();
+		choices = new ArrayList<>();
+		setChoices();
+		play();
 		
 	}
 	public static void setChoices()
 	{
 		choices = (board.getCurrentPlayer()).getPlayerHand();
+		g.hand();//have this line send the graphics class the hand of cards (choices) in order for the hand to be printed out.
+	}
+	public static void play()
+	{
+		while(board.gameRunning())
+		{
+			int choice = g.getChoice();//gets the card choice from graphics
+			board.play(choice);
+			setChoices();
+		}
+		g.setWinner(board.getWinner()); //send the graphics class the player number for displaying it 
 	}
 
 }
