d3788a9a06afaf991e2c0caf784db9685a6fc6c1
GuhanPeriyasamy <57369215+GuhanPeriyasamy@users.noreply.github.com>
2019-12-09 21:04:09 -0600

    Update Runner.java

--- Runner.java
+++ Runner.java
@@ -2,38 +2,42 @@ import java.io.IOException;
 import java.util.*;
 public class Runner {
 	private static Board board;
-	private static ArrayList<Card> choices;
+	//private static ArrayList<Card> choices;
 	private static GraphicsClass g;
 	public static void main(String args[]) throws IOException
 	{
 		board = new Board();
 		g = new GraphicsClass();
-		choices = new ArrayList<>();
-		setChoices();
+		//choices = new ArrayList<>();
+		//setChoices();
 		play();
 		
 	}
-	public static void setChoices()
+	/*public static void setChoices()
 	{
 		choices = (board.getCurrentPlayer()).getPlayerHand();
 	//	g.hand(choices);//have this line send the graphics class the hand of cards (choices) in order for the hand to be printed out.
-	}
+	}*/
 	public static void play()
 	{
-		while(board.gameRunning())
+		for(int i = 0 ; i<3; i++)
 		{
-			int choice = g.playerChoice();//gets the card choice from graphics
-			board.play(choice, g.getPlayed(), g.getBuilt());
-			Player player = board.getPreviousPlayer();
-			/*g.setCoins(player.getNumCoins());//send amount of coins
-			g.point(player.getMilitaryVictories(),player.getMilitaryDefeats(), player.getMilitaryDefeats(), player.getMilitaryPoints(), player.getVictoryPoints()); // send military stuff
-			g.*/
-			g.setAge(board.getCurrentAge());
-			g.setCurrentPlayer(player); //send player object
-			Player nextPlayer = board.getCurrentPlayer();
-			g.setNextPlayer(nextPlayer);
-			Player PrevPlayer = board.getNextPlayer();
-			g.setPreviousPlayer(PrevPlayer);
+			for(int k = 0; k<18; k++)
+			{
+				int choice = g.playerChoice();//gets the card choice from graphics
+				board.play(choice, g.getPlayed(), g.getBuilt());
+				Player player = board.getPreviousPlayer();
+				/*g.setCoins(player.getNumCoins());//send amount of coins
+				g.point(player.getMilitaryVictories(),player.getMilitaryDefeats(), player.getMilitaryDefeats(), player.getMilitaryPoints(), player.getVictoryPoints()); // send military stuff
+				g.*/
+				g.setAge(board.getCurrentAge());
+				g.setCurrentPlayer(player); //send player object
+				Player nextPlayer = board.getCurrentPlayer();
+				g.setNextPlayer(nextPlayer);
+				Player PrevPlayer = board.getNextPlayer();
+				g.setPreviousPlayer(PrevPlayer);
+			}
+			board.endOfAge();
 		}
 		g.setWinner(board.declareWinner()); //send the graphics class the player number for displaying it 
 	}
