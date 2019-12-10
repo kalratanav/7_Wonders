ac7bd67bb7191a2e755bbf455f4bd3497fc76cda
kalratanav <57369175+kalratanav@users.noreply.github.com>
2019-12-10 17:10:35 -0600

    Update Runner.java

--- Runner.java
+++ Runner.java
@@ -7,39 +7,41 @@ public class Runner {
 	public static void main(String args[]) throws IOException
 	{
 		board = new Board();
-		g = new GraphicsClass();
+		//g = new GraphicsClass();
 		//choices = new ArrayList<>();
 		//setChoices();
 		play();
 		
 	}
-	/*public static void setChoices()
+	public static void setChoices()
 	{
-		choices = (board.getCurrentPlayer()).getPlayerHand();
-	//	g.hand(choices);//have this line send the graphics class the hand of cards (choices) in order for the hand to be printed out.
-	}*/
+		//choices = (board.getCurrentPlayer()).getPlayerHand();
+		//g.hand(choices);//have this line send the graphics class the hand of cards (choices) in order for the hand to be printed out.
+	}
 	public static void play()
 	{
 		for(int i = 0 ; i<3; i++)
 		{
 			for(int k = 0; k<18; k++)
 			{
-				Player player = board.getPreviousPlayer();
-				g.setAge(board.getCurrentAge());
-				g.setCurrentPlayer(player); //send player object
-				Player nextPlayer = board.getCurrentPlayer();
+				board.printBoard();
+				/*Player player = board.getPreviousPlayer();
+				//g.setAge(board.getCurrentAge());
+				//g.setCurrentPlayer(player); //send player object
+				/*Player nextPlayer = board.getCurrentPlayer();
 				g.setNextPlayer(nextPlayer);
 				Player PrevPlayer = board.getNextPlayer();
 				g.setPreviousPlayer(PrevPlayer);
+				g.repaint();
 				if(g.getDecission()) {
 					int choice = g.playerChoice();//gets the card choice from graphics
-					board.play(choice, g.getPlayed(), g.getBuilt());
+					board.play(choice, g.getPlayed(), g.getBuilt());*/
 				}
+			board.endOfAge();
 				
 			}
-			board.endOfAge();
+			//board.endOfAge();
+			
 		}
-		g.setWinner(board.declareWinner()); //send the graphics class the player number for displaying it 
-	}
-
+		//g.setWinner(board.declareWinner()); //send the graphics class the player number for displaying it 
 }
