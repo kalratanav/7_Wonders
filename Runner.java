c3142bb7b08053379ce4feaa951eba946e21d698
JacksonForrest <57369138+JacksonForrest@users.noreply.github.com>
2019-12-10 21:47:54 -0600

    Update Runner.java

--- Runner.java
+++ Runner.java
@@ -2,22 +2,42 @@ import java.io.IOException;
 import java.util.*;
 public class Runner {
 	private static Board board;
-	//private static ArrayList<Card> choices;
 	private static GraphicsClass g;
+	private static int playerTurns = 0, age;
 	public static void main(String args[]) throws IOException
 	{
 		board = new Board();
-		//g = new GraphicsClass();
-		//choices = new ArrayList<>();
-		//setChoices();
 		play();
 		
 	}
-	public static void setChoices()
-	{
-		//choices = (board.getCurrentPlayer()).getPlayerHand();
-		//g.hand(choices);//have this line send the graphics class the hand of cards (choices) in order for the hand to be printed out.
+	
+	public static void play() {
+		System.out.println("ys");
+		GraphicsClass g = new GraphicsClass(board.getCurrentAge(), board.getCurrentPlayer(), board.getNextPlayer(), board.getPreviousPlayer());
+		while(g.getDecission() == false)
+			System.out.print(" ");
+		if(g.getDecission() == true) {
+			System.out.println("TRUE");
+			playerTurns++;
+			board.play(g.playerChoice(), g.getPlayed(),g.getBuilt());
+			System.out.println(board.getCurrentAge());
+			if(board.getCurrentAge() < 4) {		
+				if(playerTurns < 18) {
+					g.visible();
+					play();
+				}
+				else {
+					playerTurns = 0;
+					g.visible();
+					board.endOfAge();
+					play();
+				}		
+			}
+		}
+		
 	}
+	
+	/*
 	public static void play()
 	{
 		for(int i = 0 ; i<3; i++)
@@ -25,23 +45,13 @@ public class Runner {
 			for(int k = 0; k<18; k++)
 			{
 				board.printBoard();
-				/*Player player = board.getPreviousPlayer();
-				//g.setAge(board.getCurrentAge());
-				//g.setCurrentPlayer(player); //send player object
-				/*Player nextPlayer = board.getCurrentPlayer();
-				g.setNextPlayer(nextPlayer);
-				Player PrevPlayer = board.getNextPlayer();
-				g.setPreviousPlayer(PrevPlayer);
-				g.repaint();
-				if(g.getDecission()) {
-					int choice = g.playerChoice();//gets the card choice from graphics
-					board.play(choice, g.getPlayed(), g.getBuilt());*/
-				}
+				
+			}
 			board.endOfAge();
 				
 			}
 			//board.endOfAge();
 			
-		}
+		}*/
 		//g.setWinner(board.declareWinner()); //send the graphics class the player number for displaying it 
 }
